d8a3649548d609c917d5b3fe34f75771c93a26a6
Israel Ferreira <israelvf@gmail.com>
2020-03-04 19:07:41 -0300

    Adding a connection with mongodb using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Starting app
 const app = express();
 
+// Connecting with DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// First route
 app.get("/", (req, res) => {
   res.send("Hello Rocketseat");
 });
 
+// Starting web server
 app.listen(3001);
